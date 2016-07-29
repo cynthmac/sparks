7aa2386717527ccc6f375fa9c2a2bb2a1f1e39be
Jen Goree <jgoree@concord.org>
2016-07-29 09:56:21 -0400

    Removed forward slash to fix errors of not being able to load activity

--- client-breadboard-activity.js
+++ client-breadboard-activity.js
@@ -15632,7 +15632,7 @@ sparks.GAHelper.userVisitedTutorial = function (tutorialId) {
 
 (function () {
 
-  sparks.activity_base_url = "/sparks-activities/";
+  sparks.activity_base_url = "sparks-activities/";
   sparks.activity_images_base_url = "/sparks-activities/images/";
   sparks.tutorial_base_url = "tutorials/";
   sparks.soundFiles = {click: "common/sounds/click.ogg"};
